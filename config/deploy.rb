78381490ae753301edf4749d2888c0327af1129f
sankareswari <sankareswari92@gmail.com>
2020-09-20 03:29:07 +0000

    updated changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,17 @@
 lock "~> 3.14.1"
 
 set :application, "mysql_app"
-set :repo_url, "/mysql_app"
+set :repo_url, "https://github.com/sankareswari92/mysql_app.git"
 set :use_sudo, true
 set :passenger_restart_with_touch, true
 set :puma_threads,    [4, 16]
 set :ssh_options, { :forward_agent => true }
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+ set :deploy_to, "/home/user/deploy"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
